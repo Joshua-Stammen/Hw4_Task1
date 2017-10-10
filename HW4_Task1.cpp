efaee5ca62fc0d6a6d25976aa2af420d59a84d14
juan <juanrangel1@mail.weber.edu>
2017-10-10 13:14:37 -0600

    working in class

--- HW4_Task1.cpp
+++ HW4_Task1.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 class HouseInfo
 {
-
+private:
+    int streetNumber;
+    double housePrice;
+    string streetName;
+public:
+    void update()
+    {
+        cout << "\nEnter the street number: ";
+        cin >> streetNumber;
+        cout << "\nEnter the house price: ";
+        cin >> housePrice;
+        cout << "\nEnter the street name: ";
+        cin >> streetName;
+    }
 };
 
 int main() {
